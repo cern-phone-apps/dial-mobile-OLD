f66c7fc59d597fd9cebac0d234fedfcf558479a5
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-25 10:48:10 +0200

    Add StackNavigator and Tabs

--- App.js
+++ App.js
@@ -1,25 +1,50 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
+import { PropTypes } from 'prop-types'
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render () {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Home!</Text>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+class SettingsScreen extends React.Component {
+  render () {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Settings</Text>
+      </View>
+    )
   }
+}
+
+const tabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen,
 })
+
+const MyStack = createStackNavigator(
+  {
+    Root: tabNavigator
+  },
+  {
+    headerMode: 'none'
+  }
+)
+
+class CustomNavigator extends React.Component {
+  static router = MyStack.router
+
+  render () {
+    const {navigation} = this.props
+
+    return <MyStack navigation={navigation}/>
+  }
+}
+
+export default CustomNavigator
