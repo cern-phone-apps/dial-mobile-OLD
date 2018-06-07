b2df4a6e1ea37d027f39f73bb515d825db6cd288
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-06-07 16:35:05 +0200

    Add testst for refresh token

--- __tests__/reducers/auth.test.js
+++ __tests__/reducers/auth.test.js
@@ -1,15 +1,45 @@
-import { accessToken } from 'src/reducers/auth'
+import { accessToken, refreshToken } from 'src/reducers/auth'
 
 describe('Auth reducer tests', () => {
 
-  it('should dispatch LOGIN_SUCCESS when login is called', () => {
+  it('accessToken not defined', () => {
     const state = {
       auth: {
         access: {}
       }
     }
-
     expect(accessToken(state)).toEqual(undefined)
+  })
+
+  it('accessToken defined', () => {
+    const state = {
+      auth: {
+        access: {
+          token: '123456'
+        }
+      }
+    }
+    expect(accessToken(state)).toEqual('123456')
+  })
+
+  it('refreshToken not defined', () => {
+    const state = {
+      auth: {
+        refresh: {}
+      }
+    }
+    expect(refreshToken(state)).toEqual(undefined)
+  })
+
+  it('refreshToken defined', () => {
+    const state = {
+      auth: {
+        refresh: {
+          token: '123456'
+        }
+      }
+    }
+    expect(refreshToken(state)).toEqual('123456')
   })
 
 })
