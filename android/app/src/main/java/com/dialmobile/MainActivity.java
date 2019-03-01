28d05c93244dcf35cd7f3ee12afb38e19f2975ff
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-03-01 15:38:15 +0100

    Add support for react-navigation

--- android/app/src/main/java/com/dialmobile/MainActivity.java
+++ android/app/src/main/java/com/dialmobile/MainActivity.java
@@ -2,6 +2,12 @@ package com.dialmobile;
 
 import com.facebook.react.ReactActivity;
 
+// START react-navigation
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+// END react-navigation
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +17,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "DialMobile";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
