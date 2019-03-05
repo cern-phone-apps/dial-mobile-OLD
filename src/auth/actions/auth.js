60f12449431169d375007d77616b407ababab1ef
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-03-05 10:54:00 +0100

    Add auth actions and utils

---
+++ src/auth/actions/auth.js
@@ -0,0 +1,13 @@
+export const LOGIN_REQUEST = "@@auth/LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "@@auth/LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "@@auth/LOGIN_FAILURE";
+
+export const LOGOUT_REQUEST = "@@auth/LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "@@auth/LOGOUT_SUCCESS";
+export const LOGOUT_FAILURE = "@@auth/LOGOUT_FAILURE";
+
+export const TOKEN_REQUEST = "@@auth/TOKEN_REQUEST";
+export const TOKEN_RECEIVED = "@@auth/TOKEN_RECEIVED";
+export const TOKEN_FAILURE = "@@auth/TOKEN_FAILURE";
+
+export const CLEAR_TOKEN = "@@auth/CLEAR_TOKEN";
