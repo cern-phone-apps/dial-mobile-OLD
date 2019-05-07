4cb280b9ff0d1847384390239b79495602f577ae
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-05-07 13:46:18 +0200

    AuthStack: style fixes

--- mobile/App.js
+++ mobile/App.js
@@ -11,7 +11,7 @@ import React from "react";
 
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import AuthLoadingScreenContainer from "./src/auth/screens/AuthLoadingScreen/AuthLoadingScreenContainer";
-import { AuthStack } from "./src/auth/navigators/auth";
+import AuthStack from "./src/auth/navigators/auth";
 import { AppFullStack } from "./src/calls/navigators";
 
 export default createAppContainer(
--- mobile/src/auth/navigators/auth.js
+++ mobile/src/auth/navigators/auth.js
@@ -1,36 +1,34 @@
-import { createStackNavigator } from "react-navigation";
-import LoginScreenContainer from "../screens/LoginScreen/LoginScreenContainer";
-import LoginWebViewContainer from "../screens/LoginWebView/LoginWebViewContainer";
+import { createStackNavigator } from 'react-navigation';
+import LoginScreenContainer from '../screens/LoginScreen/LoginScreenContainer';
+import LoginWebViewContainer from '../screens/LoginWebView/LoginWebViewContainer';
 
 export const AuthStack = createStackNavigator({
   SignIn: {
     screen: LoginScreenContainer,
-    navigationOptions: () => {
-      return {
-        headerTitleStyle: {
-          color: "white"
-        },
-        title: `Sign in`,
-        headerStyle: {
-          backgroundColor: "#2196F3"
-        },
-        headerTintColor: "white"
-      };
-    }
+    navigationOptions: () => ({
+      headerTitleStyle: {
+        color: 'white'
+      },
+      title: `Sign in`,
+      headerStyle: {
+        backgroundColor: '#2196F3'
+      },
+      headerTintColor: 'white'
+    })
   },
   LoginWebView: {
     screen: LoginWebViewContainer,
-    navigationOptions: () => {
-      return {
-        headerTitleStyle: {
-          color: "white"
-        },
-        title: `Input your credentials`,
-        headerStyle: {
-          backgroundColor: "#2196F3"
-        },
-        headerTintColor: "white"
-      };
-    }
+    navigationOptions: () => ({
+      headerTitleStyle: {
+        color: 'white'
+      },
+      title: `Input your credentials`,
+      headerStyle: {
+        backgroundColor: '#2196F3'
+      },
+      headerTintColor: 'white'
+    })
   }
 });
+
+export default AuthStack;
