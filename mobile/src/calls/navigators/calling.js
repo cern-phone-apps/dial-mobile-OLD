83979465ffe31aa4bbf8fd9c56109a2b556c19b3
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-05-09 15:53:32 +0200

    Create calling.js

---
+++ mobile/src/calls/navigators/calling.js
@@ -0,0 +1,6 @@
+import { createStackNavigator } from 'react-navigation';
+import CallingScreen from '../screens/CallingScreen/CallingScreen';
+
+export const CallingStack = createStackNavigator({
+  Calling: CallingScreen
+});
