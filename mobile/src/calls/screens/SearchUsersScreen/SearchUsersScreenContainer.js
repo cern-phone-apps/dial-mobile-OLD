bfc126b4e79b87231bfe08c3bcb46236fe35ba4f
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2019-05-10 11:20:43 +0200

    Add SearchUsers screen

---
+++ mobile/src/calls/screens/SearchUsersScreen/SearchUsersScreenContainer.js
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { API_ENDPOINT } from 'react-native-dotenv';
+
+import { usersActionsFactory, contactsActionsFactory } from 'dial-core';
+
+import SearchUsersScreen from './SearchUsersScreen';
+
+const contactsActions = contactsActionsFactory(API_ENDPOINT);
+const usersActions = usersActionsFactory(API_ENDPOINT);
+
+export default connect(
+  ({ users, contacts: { contacts } }) => ({
+    ...users,
+    contacts
+  }),
+  dispatch =>
+    bindActionCreators(
+      {
+        ...usersActions,
+        addUserContact: contactsActions.addUserContact,
+        getUserContacts: contactsActions.getUserContacts
+      },
+      dispatch
+    )
+)(SearchUsersScreen);
